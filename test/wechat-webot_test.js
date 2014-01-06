24a7edc6c4553480dba0e7851e7645fc2002a5d8
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-01-06 20:52:02 +0800

    Use mocha BDD style test

--- test/wechat-webot_test.js
+++ test/wechat-webot_test.js
@@ -1,36 +1,11 @@
 'use strict';
 
-var wechat-webot = require('../lib/wechat-webot.js');
+var wechatWebot = require('../lib/wechat-webot.js');
 
-/*
-  ======== A Handy Little Mocha Reference ========
-  https://github.com/visionmedia/mocha/
+require('should');
 
-  Test assertions:
-    assert.fail(actual, expected, message, operator)
-    assert(value, message), assert.ok(value, [message])
-    assert.equal(actual, expected, [message])
-    assert.notEqual(actual, expected, [message])
-    assert.deepEqual(actual, expected, [message])
-    assert.notDeepEqual(actual, expected, [message])
-    assert.strictEqual(actual, expected, [message])
-    assert.notStrictEqual(actual, expected, [message])
-    assert.throws(block, [error], [message])
-    assert.doesNotThrow(block, [message])
-    assert.ifError(value)
-
-    Apart from assert, Mocha allows you to use any of the following assertion libraries:
-    - should.js
-    - chai
-    - expect.js
-    - better-assert
-*/
-
-var assert = require('assert');
-
-suite('AwesomenessTest', function(){
-  test('#awesome()', function(done){
-    assert.equal(wechat-webot.awesome(), 'awesome');
-    done();
+describe('wechat-webot', function () {
+  it('should export function', function () {
+    wechatWebot.should.be.type('function');
   });
 });
