d704b93a13959132a33650c356a8f270eb4283ef
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-01-06 20:58:45 +0800

    Add test script

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,8 +10,8 @@ module.exports = function (grunt) {
   grunt.initConfig({
     mochaTest: {
      options: {
-	ui: 'tdd'
-     }, 
+	ui: 'bdd'
+     },
      test: {
         src: ['test/**/*.js']
       }
